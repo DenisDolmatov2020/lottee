440ee8d1a2dd93e7c678fa344a71c03504b3092c
denisdolmatov2020 <denisdolmatov2020@gmail.com>
2020-11-15 14:35:06 +0700

    Update settings.py

--- lottee/settings.py
+++ lottee/settings.py
@@ -89,8 +89,12 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Lottee',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
