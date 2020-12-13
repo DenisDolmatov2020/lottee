b11d906cd86ec1b2b36c27872b92cc58b75e378a
denisdolmatov2020 <denisdolmatov2020@gmail.com>
2020-12-13 07:02:39 +0700

    123

--- lottee/settings.py
+++ lottee/settings.py
@@ -79,15 +79,18 @@ AUTH_USER_MODEL = 'my_user.User'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-
+if os.environ.get("POSTGRES_ENGINE"):
+    print('T')
+else:
+    print('TTT')
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": 'root',  # os.environ.get("SQL_PASSWORD", "root"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        "ENGINE": os.environ.get("POSTGRES_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("POSTGRES_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
